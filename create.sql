3c4c3c77b961e8e0935b863214ccc49fa273d492
hyzerki <hyzerki@gmail.com>
2023-03-07 12:01:48 +0300

    Created database creation script

---
+++ create.sql
@@ -0,0 +1,40 @@
+create table users
+(
+	id serial primary key,
+	user_name varchar(20) unique not null check (char_length(userName)>=5)
+	rating smallint default(1000) not null,
+	password bytea not null,
+	salt bytea not null
+);
+
+create type text_language as enum('russian', 'english');
+
+create table texts
+(
+	id serial primary key,
+	text text not null unique check(text != ''),
+	text_language text_language not null,
+	text_length smallint
+);
+
+create type game_type as enum('casual', 'ranked', 'training');
+
+create table games
+(
+	id bigserial primary key,
+	type game_type not null,
+	date_time timestamp default current_timestamp not null,
+	text_id serial references texts(id) not null
+);
+
+create table player_game_stats
+(
+	game_id serial references games(id),
+	player_id serial references users(id),
+	rating smallint,
+	rating_gain smallint,
+	words_per_minute smallint,
+	accuracy numeric(3,2),
+	typing_time interval,
+	primary key(matchId, playerId)
+);
